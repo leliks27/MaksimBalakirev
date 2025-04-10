00a42565582facbf9466099a94fabc3d50e8f390
leliks27 <masiany2711@gmail.com>
2025-04-10 21:56:46 +0500

    Delete individ plan directory

--- individ plan/12/19244.py
+++
@@ -1,9 +0,0 @@
-for n in range (4, 10000):
-    s = '1' + '2' * n
-    while '12' in s or '322' in s or '222' in s:
-        s = s.replace('12', '2', 1)
-        s = s.replace('322', '21', 1)
-        s = s.replace('222', '3', 1)
-    if sum(map(int, s)) == 15:
-        print(n)
-        break
--- individ plan/2/6843.py
+++
@@ -1,8 +0,0 @@
-print('x y z w f')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f = ((z <= w) and y and (not x))
-                print(x, y, z, w, int(f))
-#zwyx
